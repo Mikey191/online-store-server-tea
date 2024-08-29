f7ae23172736b4c8ec39673dfb31728dfc4dafd9
Mikey191 <desertigorb@gmail.com>
2024-08-29 11:40:59 +0300

    connect db

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./db.js");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started in PORT {PORT}`));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
