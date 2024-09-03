e56cd84ca40468b7fe439c6b2d22a1118543a82c
Mikey191 <desertigorb@gmail.com>
2024-09-03 12:30:51 +0300

    ErrorHandler

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const sequelize = require("./db.js");
 const models = require("./models/models");
 const cors = require("cors");
 const router = require("./routes/index.js");
+const errorHandler = require("./middleware/ErrorHandlingMiddleware.js")
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
-app.use(express.json()); // для парсинга json формата
+app.use(express.json());
 app.use("/api", router);
 
 const start = async () => {
